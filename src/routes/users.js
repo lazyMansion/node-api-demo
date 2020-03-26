2e7ecfc2cfd409ccc0f06db909f9b85f2b4bbbfd
zhangzhiqiang <zhangzhiqiang@jzic.com>
2020-03-26 13:52:31 +0800

    更改项目结构，重新初始化项目

--- src/routes/users.js
+++
@@ -1,9 +0,0 @@
-const router = require('koa-router')()
-const UserController = require('../controllers/user');
-
-//创建用户
-router.post('/user/create',UserController.create);
-router.get('/user/detail',UserController.detail);
-router.post('/user/login',UserController.login);
-
-module.exports = router
